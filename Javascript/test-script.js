a4133627d05a53da632ba58813a17dd52c2a29d7
Jeffrey Slentz <jcszk9@umkc.edu>
2017-07-05 21:11:05 -0500

    Split code into functions

--- Javascript/test-script.js
+++ Javascript/test-script.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
 
-  var jsonData;
+var jsonData;
+var jSonStop;
 
-  $.getJSON('https://api.myjson.com/bins/oz9kn', function(data) {
+  $.getJSON('https://api.myjson.com/bins/64n67', function(data) {
     //data is the JSON string
     jsonData = data;
+    console.log(jsonData[1]);
+
+    var obj = {"Name": "Raj", "Something": "lol"};
+
+    console.log(obj);
   });
+
+  $.getJSON('https://api.myjson.com/bins/qddqn', function(nData){
+      //nData is the JSON string of stopwords
+      jSonStop = nData;
+
+  });
+
 
   //assign the function filterResults to the searchBar
   $("#searchBar").keyup(filterResults);
@@ -20,16 +33,24 @@ $(document).ready(function(){
       //Clear the dropdown.
       $("#resultsDropdown").empty();
 
+
+      //searchArray = ["fly", "fishing"]                                    //Regular Array
+      //searchPermutations = [ ["fly", "fishing"], ["fly"], ["fishing"] ]   //2D Array
+      var searchArray = splitAndRemoveExtraWords(searchString);
+      var searchPermutations = makePermutations(searchArray);
+
+      var searchResults = search(searchPermutations);
+
       var matches = 0;
+
       //Iterate through the jsonData.
       $.each(jsonData, function( index, value ) {
         //this part is rough to maintain if the data-structure changes.
-        var companyName = value["company"].toUpperCase();
-
-        if (value["company"].includes(searchString)){
+        var title = value["Title"].toUpperCase();
+        if (title.includes(searchString)){
           matches++;
           //This is naive, we'll need to capture related suggestions and sort them by relatibilty later on.
-          $("#resultsDropdown").append('<li ><a href="#">' + value["company"] + '</a></li>');
+          $("#resultsDropdown").append('<li ><a href="#">' + value["Title"] + '</a></li>');
         }
 
         if (matches > 5){
@@ -39,4 +60,50 @@ $(document).ready(function(){
       $("#resultsDropdown").show();
     }
   }
+
+  function splitAndRemoveExtraWords(searchString){
+      var searchArray = searchString.split(" ");
+
+      //i feel like there is a much more efficient way to accomplish this.
+      for(var i = 0; i < searchArray.length; i++){
+          if(jSonStop.includes(searchArray [i])){
+              searchArray.splice(i, 1);
+          }
+      }
+
+      return searchArray;
+  }
+
+  function makePermutations(searchArray){
+      //TODO: Implement
+
+
+      //This just turns it into a 2d array with one entry.
+      var searchPermutations = [searchArray];
+      return searchPermutations;
+  }
+
+    function search(searchPermutations){
+
+        var score;
+
+
+        $.each(jsonData, function(i, jsonEntry) {
+            $.each(searchPermutations, function( j, searchArray ) {
+                $.each(searchArray, function( j, searchTerm ) {
+                    var matchFound = false;
+
+                    if(jsonEntry["Title"].includes(searchTerm)){
+                        matchFound = true;
+                        //TODO: calculate a score for matching the title
+                    }
+
+                    if(jsonEntry["Summary"].includes(searchTerm)){
+                        matchfound
+                        //TODO: calculate a score for matching the description.
+                    }
+                });
+            });
+        });
+    }
 });
